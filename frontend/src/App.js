e0b47100ad999df1e9ceaee6fa86f233cd9b4ffa
Teja3993 <karritej2@gmail.com>
2025-11-22 22:56:48 +0530

    AC-29: Created search box and header component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,21 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Routes, Route } from 'react-router-dom';
+import Header from './components/Header'; // <--- NEW
 import HomeScreen from './screens/HomeScreen';
 import ProfileScreen from './screens/ProfileScreen';
-import ProductScreen from './screens/ProductScreen'; // <--- Import New Screen
+import ProductScreen from './screens/ProductScreen';
 
 function App() {
   return (
     <div className="App">
+      <Header />  {/* <--- ADD THIS */}
       <main className='py-3'>
         <Container>
           <Routes>
             <Route path='/' element={<HomeScreen />} />
             <Route path='/profile' element={<ProfileScreen />} />
-            <Route path='/product/:id' element={<ProductScreen />} /> {/* <--- Add Route */}
+            <Route path='/product/:id' element={<ProductScreen />} />
           </Routes>
         </Container>
       </main>
