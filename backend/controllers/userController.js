23016dfc4d15a5ea0b30b9c55ecf9f2af0b29bb4
Teja3993 <karritej2@gmail.com>
2025-11-22 18:59:58 +0530

    AC-17: implemented login logic and password comparison

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,7 @@
 const asyncHandler = require('express-async-handler');
 const User = require('../models/userModel');
 const bcrypt = require('bcryptjs');
-
+const generateToken = require('../utils/generateToken');
 // @desc    Register a new user
 // @route   POST /api/users
 // @access  Public
@@ -38,5 +38,26 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error('Invalid user data');
   }
 });
+// @desc    Auth user & get token
+// @route   POST /api/users/login
+// @access  Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
-module.exports = { registerUser };
+  const user = await User.findOne({ email });
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    });
+  } else {
+    res.status(401);
+    throw new Error('Invalid email or password');
+  }
+});
+
+module.exports = { registerUser, authUser };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { registerUser } = require('../controllers/userController');
-
+const { registerUser, authUser } = require('../controllers/userController');
 router.post('/', registerUser);
-
+router.post('/login', authUser);
 module.exports = router;
