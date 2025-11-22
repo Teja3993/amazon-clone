1d9949503320ccec4ee6e1174df6c949f153eaff
Teja3993 <karritej2@gmail.com>
2025-11-22 18:10:46 +0530

    AC-14: Built sign-up API, setup server and db

---
+++ backend/controllers/userController.js
@@ -0,0 +1,42 @@
+const asyncHandler = require('express-async-handler');
+const User = require('../models/userModel');
+const bcrypt = require('bcryptjs');
+
+// @desc    Register a new user
+// @route   POST /api/users
+// @access  Public
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body;
+
+  // 1. Check if user already exists
+  const userExists = await User.findOne({ email });
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exists');
+  }
+
+  // 2. Hash password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // 3. Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
+});
+
+module.exports = { registerUser };
