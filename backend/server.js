03d34fab4c56899bd2c9032c05146ec026f8b71e
Teja3993 <karritej2@gmail.com>
2025-11-22 20:12:00 +0530

    AC-23: Created product API endpoints

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const userRoutes = require('./routes/userRoutes');
-
+const productRoutes = require('./routes/productRoutes');
 dotenv.config();
 connectDB();
 
@@ -11,7 +11,7 @@ const app = express();
 app.use(express.json()); // Allows us to accept JSON data in the body
 
 app.use('/api/users', userRoutes);
-
+app.use('/api/products', productRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
