b193b8c95afd09fcc517e89777d9a8c1129eeffb
Teja3993 <karritej2@gmail.com>
2025-11-23 13:25:46 +0530

    AC-47: Updated product data paths and re-seeded

--- backend/data/products.js
+++ backend/data/products.js
@@ -1,7 +1,7 @@
 const products = [
   {
     name: 'Airpods Wireless Bluetooth Headphones',
-    image: '/images/airpods.jpg',
+    image: '/images/airpods.jpg', // Ensure you named the file airpods.jpg
     description: 'Bluetooth technology lets you connect it with compatible devices wirelessly',
     brand: 'Apple',
     category: 'Electronics',
@@ -12,7 +12,7 @@ const products = [
   },
   {
     name: 'iPhone 11 Pro 256GB Memory',
-    image: '/images/phone.jpg',
+    image: '/images/phone.jpg', // Ensure you named the file phone.jpg
     description: 'Introducing the iPhone 11 Pro. A transformative triple-camera system that adds tons of capability without complexity',
     brand: 'Apple',
     category: 'Electronics',
@@ -23,7 +23,7 @@ const products = [
   },
   {
     name: 'Cannon EOS 80D DSLR Camera',
-    image: '/images/camera.jpg',
+    image: '/images/camera.jpg', // Ensure you named the file camera.jpg
     description: 'Characterized by versatile imaging specs, the Canon EOS 80D further clarifies itself using a pair of robust focusing systems',
     brand: 'Cannon',
     category: 'Electronics',
@@ -34,13 +34,35 @@ const products = [
   },
   {
     name: 'Sony Playstation 4 Pro White Version',
-    image: '/images/playstation.jpg',
+    image: '/images/playstation.jpg', // Ensure you named the file playstation.jpg
     description: 'The ultimate home entertainment center starts with PlayStation. Whether you are into gaming, HD movies, television, music',
     brand: 'Sony',
     category: 'Electronics',
     price: 399.99,
     countInStock: 11,
     rating: 5,
+    numReviews: 12,
+  },
+  {
+    name: 'Logitech G-Series Gaming Mouse',
+    image: '/images/mouse.jpg', // Ensure you named the file mouse.jpg
+    description: 'Get a better handle on your games with this Logitech LIGHTSYNC gaming mouse.',
+    brand: 'Logitech',
+    category: 'Electronics',
+    price: 49.99,
+    countInStock: 7,
+    rating: 3.5,
+    numReviews: 10,
+  },
+  {
+    name: 'Amazon Echo Dot 3rd Generation',
+    image: '/images/alexa.jpg', // Ensure you named the file alexa.jpg
+    description: 'Meet Echo Dot - Our most popular smart speaker with a fabric design. It is our most compact smart speaker that fits perfectly into small space',
+    brand: 'Amazon',
+    category: 'Electronics',
+    price: 29.99,
+    countInStock: 0,
+    rating: 4,
     numReviews: 12,
   },
 ];
